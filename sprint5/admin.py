db318450ae48fa76dcea66f7369039eb7d9ee70f
liege-fontenele <liege.fontenele@protonmail.com>
2026-05-29 03:25:25 -0300

    criado para popular banco de dados

--- sprint5/admin.py
+++ sprint5/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Posto
 
+@admin.register(Posto)
+class PostoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'bandeira', 'endereco', 'avaliacao']
+    list_filter = ['bandeira']
+    search_fields = ['nome', 'endereco']
 # Register your models here.
