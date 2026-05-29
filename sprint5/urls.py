f35e8fc88895d41ca5856f2dbae1a8af2f96480d
liege-fontenele <liege.fontenele@protonmail.com>
2026-05-29 02:10:36 -0300

    alterações baseadas no repositorio antigo

--- sprint5/urls.py
+++ sprint5/urls.py
@@ -7,11 +7,11 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home)
+    ]
+
      # Quando a URL nao for /admin, manda pro app postos
     #path('', include('postos.urls')), #isso tá na primeira parte do projeto
     """path('', views.login_view, name='login'),
     path('home/', views.home_view, name='home'),
     path('posto/<int:posto_id>/', views.detalhes_view, name='detalhes'),
-    path('perfil/', views.perfil_view, name='perfil'),
-"""
-]
+    path('perfil/', views.perfil_view, name='perfil'),"""
